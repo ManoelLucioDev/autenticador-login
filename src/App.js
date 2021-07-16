dae06c004d06f910782c0466faf3f1e3ecb0618e
MANOEL <manoel.programer@gmail.com>
2021-07-16 09:14:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import style from 'styled-components';
 import './App.css';
 
+const Input = style.input`
+  margin-top: 20px;
+  width:400px;
+  height: 30px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid #000;
+`;
+
 function App() {
+
+  const [texto, setTexto] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailInput = (e) => {
+    setTexto(e.target.value);
+  }
+
+  const handlePasswordInput = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const emailLogin = "manoel.programer@gmail.com";
+  const senhaLogin = 123;
+
+  const verificar = () => {
+    if( `${texto }` == emailLogin  && `${password}` == senhaLogin){
+      alert("Logado");
+    } 
+
+    return alert("LOGIN ou SENHA INVALIDOS")
+  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Input placeholder="email" type="email"  onChange={handleEmailInput} />
+      <Input placeholder="senha" type="password"  onChange={handlePasswordInput} />
+      <button  onClick={verificar}>LOGAR</button>
+    
+    
+    
+    </>
   );
 }
 
